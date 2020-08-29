fb49363eb3a914fe03822de0deb46e31061242e1
linncode <193265261@qq.com>
2020-08-29 16:02:16 +0800

    实现跨域登录

--- ajax进阶/server2/s2/app.js
+++ ajax进阶/server2/s2/app.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const path = require('path')
 const formidable = require('formidable')
+const session = require('express-session')
 const app = express()
+
+// 实现session功能
+app.use(session({
+	secret: 'keyboard cat',
+	resave: false,
+	saveUninitialized: false
+}));
+
 // 静态资源访问服务功能
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -41,7 +50,32 @@ app.get('/cross', (req, res) => {
 	res.send('ok')
 })
 
+app.post('/login', (req, res) => {
+	// 创建表单解析对象
+	var form = formidable.IncomingForm()
+	// 解析表单
+	form.parse(req, (err, fields, file) => {
+		// 接收客户端传递过来的用户名和密码
+		const { username, password } = fields
+		// 用户名密码比对
+		if (username == 'Linn' && password == '123') {
+			// 设置session
+			req.session.isLogin = true
+			res.send({message: '登录成功'})
+		} else {
+			res.send({message: '登录失败, 用户名或密码错误'})
+		}
+	})
+})
 
+app.get('/checkLogin', (req, res) => {
+	// 判断用户是否处于登录状态
+	if (req.session.isLogin) {
+		res.send({message: '处于登录状态'})
+	} else {
+		res.send({message: '处于未登录状态'})
+	}
+})
 
 // 监听端口
 app.listen(3001);
