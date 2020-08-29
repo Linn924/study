4eb3f7ebcfdc0e5d1fbd2a110d05579448c6bb7c
linncode <193265261@qq.com>
2020-08-29 15:37:13 +0800

    跨域访问问题

--- ajax进阶/server2/s1/app.js
+++ ajax进阶/server2/s1/app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const path = require('path')
 const formidable = require('formidable')
+//向其他服务器端请求数据的模块
+const request = require('request')
 const app = express()
 // 静态资源访问服务功能
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.get('/server', (req, res) => {
+	request('http://localhost:3001/cross', (err, response, body) => {
+		res.send(body)
+	})
+})
 
 // 监听端口
 app.listen(3000);
--- ajax进阶/server2/s2/app.js
+++ ajax进阶/server2/s2/app.js
@@ -5,6 +5,18 @@ const app = express()
 // 静态资源访问服务功能
 app.use(express.static(path.join(__dirname, 'public')))
 
+// 拦截所有请求
+app.use((req, res, next) => {
+	// 1.允许哪些客户端访问我
+	// * 代表允许所有的客户端访问我
+	// 注意：如果跨域请求中涉及到cookie信息传递，值不可以为*号 比如是具体的域名信息
+	res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+	// 2.允许客户端使用哪些请求方法访问我
+	res.header('Access-Control-Allow-Methods', 'get,post')
+	// 允许客户端发送跨域请求时携带cookie信息
+	res.header('Access-Control-Allow-Credentials', true)
+	next()
+})
 
 app.get('/test', (req, res) => {
 	const result = 'fn({name: "张三"})';
@@ -24,6 +36,12 @@ app.get('/better', (req, res) => {
 	//代替上述代码
 	res.jsonp({name: 'Linn', age: 20})
 })
+
+app.get('/cross', (req, res) => {
+	res.send('ok')
+})
+
+
 
 // 监听端口
 app.listen(3001);
