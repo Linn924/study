f635ab254302208ebd8d07b1e8e88c36f89d3aa8
linncode <193265261@qq.com>
2020-08-29 14:18:32 +0800

    封装jsonp函数

--- ajax进阶/server2/s2/app.js
+++ ajax进阶/server2/s2/app.js
@@ -9,7 +9,20 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/test', (req, res) => {
 	const result = 'fn({name: "张三"})';
 	res.send(result);
-});
+})
+
+app.get('/better', (req, res) => {
+	// 接收客户端传递过来的函数的名称
+	const fnName = req.query.callback
+	// 将函数名称对应的函数调用代码返回给客户端
+	const data = JSON.stringify({name: "张三"})
+	//拼接成fn({name:"张三"})
+	const result = fnName + '('+ data +')'
+	setTimeout(() => {
+		res.send(result)
+	}, 1000)
+	// res.jsonp({name: 'lisi', age: 20})
+})
 
 // 监听端口
 app.listen(3001);
