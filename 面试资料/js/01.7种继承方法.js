f0dce5ccd209d61340a252ed508359d4b0a51e1c
linncode <193265261@qq.com>
2020-10-14 10:17:28 +0800

    新增博客

--- 面试资料/js/01.7种继承方法.js
+++ 面试资料/js/01.7种继承方法.js
@@ -1,14 +1,14 @@
 //定义一个动物类
 function Animal(name) {
-    this.name = name || 'Animal'
+    this.name = name || "Animal"
     this.sleep = () =>{
-        console.log(this.name + '在睡觉')
+        console.log(this.name + "在睡觉")
     }
 }
 
 //在Animal原型上添加eat方法
 Animal.prototype.eat = () =>{
-    console.log(this.name + '吃鱼')
+    console.log(this.name + "吃鱼")
 }
 
 
@@ -21,19 +21,19 @@ Animal.prototype.eat = () =>{
 */
 
 
-function Cat(){
+// function Cat(){
 
-}
+// }
 
-Cat.prototype = new Animal()
-Cat.prototype.name = 'Tom'
+// Cat.prototype = new Animal()
+// Cat.prototype.name = "Tom"
 
-let cat = new Cat()
-console.log(cat.name) //Tom
-console.log(cat.eat()) //undefined吃鱼
-console.log(cat.sleep()) //Tom在睡觉
-console.log(cat instanceof Animal) //true
-console.log(cat instanceof Cat); // true
+// let cat = new Cat()
+// console.log(cat.name) //Tom
+// console.log(cat.eat()) //undefined吃鱼
+// console.log(cat.sleep()) //Tom在睡觉
+// console.log(cat instanceof Animal) //true
+// console.log(cat instanceof Cat) // true
 
 /*
     2.构造继承
@@ -43,15 +43,15 @@ console.log(cat instanceof Cat); // true
 */
 
 // function Cat(name){
-//     //this 指向 Cat { name: 'Animal', sleep: [Function] }
+//     //this 指向 Cat { name: "Animal", sleep: [Function] }
 //     Animal.call(this,name)
 //     // this.name= name
 // }
 
-// let cat = new Cat('Tom')
+// let cat = new Cat("Tom")
 // console.log(cat.name) //Tom
 // console.log(cat.sleep()) //Tom在睡觉
-// console.log(cat.eat()) //报错
+// // console.log(cat.eat()) //报错
 // // 判断 cat 是否是 Cat 类的实例 , 并且是否是其父类型的实例
 // // instanceof 方法要求开发者明确地确认对象为某特定类型。
 // console.log(cat instanceof Animal) //false
@@ -65,18 +65,18 @@ console.log(cat instanceof Cat); // true
     缺点: 实例是父类的实例，不是子类的实例;不支持多继承
 */
 
-// function Cat(name){
-//     var instance = new Animal()
-//     instance.name = name
-//     return instance
-// }
+function Cat(name){
+    var instance = new Animal()
+    instance.name = name
+    return instance
+}
 
-// let cat = new Cat('Tom')
-// console.log(cat.name) //Tom
-// console.log(cat.sleep()) //Tom在睡觉
-// console.log(cat.eat()) //undefined吃鱼
-// console.log(cat instanceof Animal) //true
-// console.log(cat instanceof Cat) //false
+let cat = new Cat("Tom")
+console.log(cat.name) //Tom
+console.log(cat.sleep()) //Tom在睡觉
+console.log(cat.eat()) //undefined吃鱼
+console.log(cat instanceof Animal) //true
+console.log(cat instanceof Cat) //false
 
 
 /**
@@ -85,21 +85,21 @@ console.log(cat instanceof Cat); // true
     缺点: 效率较低，内存占用高（因为要拷贝父类的属性;无法获取父类不可枚举的方法（不可枚举方法，不能使用for in 访问到）
 */
 
-// function Cat(name){
-//     var animal = new Animal()
-//   　// 遍历拷贝属性
-//     for(var p in animal){
-//       Cat.prototype[p] = animal[p]
-//     }
-//     Cat.prototype.name = name || 'Tom'
-// }
+function Cat(name){
+    var animal = new Animal()
+  　// 遍历拷贝属性
+    for(var p in animal){
+      Cat.prototype[p] = animal[p]
+    }
+    Cat.prototype.name = name || "Tom"
+}
 
-// let cat = new Cat()
-// console.log(cat.name) //Tom
-// console.log(cat.sleep()) //Animal在睡觉
-// console.log(cat.eat()) //undefined吃鱼
-// console.log(cat instanceof Animal) //false
-// console.log(cat instanceof Cat) //true
+let cat = new Cat()
+console.log(cat.name) //Tom
+console.log(cat.sleep()) //Animal在睡觉
+console.log(cat.eat()) //undefined吃鱼
+console.log(cat instanceof Animal) //false
+console.log(cat instanceof Cat) //true
 
 /*
     5.组合继承
@@ -108,22 +108,22 @@ console.log(cat instanceof Cat); // true
     缺点: 调用了两次父类构造函数，生成了两份实例（子类实例将子类原型上的那份屏蔽了）
 */
 
-// function Cat(name){
-//     Animal.call(this)
-//     this.name = name || 'Tom'
-// }
+function Cat(name){
+    Animal.call(this)
+    this.name = name || "Tom"
+}
 
-// Cat.prototype = new Animal()
+Cat.prototype = new Animal()
 
-// //修复构造函数指向
-// Cat.prototype.constructor = Cat
+//修复构造函数指向
+Cat.prototype.constructor = Cat
 
-// let cat = new Cat()
-// console.log(cat.name) //Tom
-// console.log(cat.sleep()) //Tom在睡觉
-// console.log(cat.eat()) //undefined吃鱼
-// console.log(cat instanceof Animal) //true
-// console.log(cat instanceof Cat) //true
+let cat = new Cat()
+console.log(cat.name) //Tom
+console.log(cat.sleep()) //Tom在睡觉
+console.log(cat.eat()) //undefined吃鱼
+console.log(cat instanceof Animal) //true
+console.log(cat instanceof Cat) //true
 
 
 /*
@@ -131,43 +131,43 @@ console.log(cat instanceof Cat); // true
     核心: 通过寄生方式，砍掉父类的实例属性，这样，在调用两次父类的构造的时候，就不会初始化两次实例方法/属性，避免的组合继承的缺点
 */
 
-// function Cat(name){
-//     Animal.call(this)
-//     this.name = name || 'Tom'
-// }
-// (function(){
-//     // 创建一个没有实例方法的类
-//     var Super = function(){};
-//     Super.prototype = Animal.prototype
-//     //将实例作为子类的原型
-//     Cat.prototype = new Super()
-// })()
+function Cat(name){
+    Animal.call(this)
+    this.name = name || "Tom"
+}
+(function(){
+    // 创建一个没有实例方法的类
+    var Super = function(){};
+    Super.prototype = Animal.prototype
+    //将实例作为子类的原型
+    Cat.prototype = new Super()
+})()
   
  
-// let cat = new Cat()
-// console.log(cat.name) //Tom
-// console.log(cat.sleep()) //Tom在睡觉
-// console.log(cat.eat()) //undefined吃鱼
-// console.log(cat instanceof Animal) //true
-// console.log(cat instanceof Cat) //true
+let cat = new Cat()
+console.log(cat.name) //Tom
+console.log(cat.sleep()) //Tom在睡觉
+console.log(cat.eat()) //undefined吃鱼
+console.log(cat instanceof Animal) //true
+console.log(cat instanceof Cat) //true
 
-// Cat.prototype.constructor = Cat // 需要修复下构造函数
+Cat.prototype.constructor = Cat // 需要修复下构造函数
 
 /*
     7.Class 继承
     核心: 使用 extends 表明继承自哪个父类，并且在子类构造函数中必须调用 super，这段代码可以看成 Animal.call(this, name)。Class 的本质就是函数
 */
 
-// class Cat extends Animal{
-//     constructor(name){
-//         super(name)
-//         this.name = name || 'Tom'
-//     }
-// }
+class Cat extends Animal{
+    constructor(name){
+        super(name)
+        this.name = name || "Tom"
+    }
+}
 
-// let cat = new Cat()
-// console.log(cat.name) //Tom
-// console.log(cat.sleep()) //Tom在睡觉
-// console.log(cat.eat()) //undefined吃鱼
-// console.log(cat instanceof Animal) // true
-// console.log(cat instanceof Cat) //true
+let cat = new Cat()
+console.log(cat.name) //Tom
+console.log(cat.sleep()) //Tom在睡觉
+console.log(cat.eat()) //undefined吃鱼
+console.log(cat instanceof Animal) // true
+console.log(cat instanceof Cat) //true
