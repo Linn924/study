de83d6cc283f82639d477a559640afde25bbe185
linncode <193265261@qq.com>
2020-10-14 22:20:05 +0800

    整理资料

--- 面试资料/js/test.js
+++ 面试资料/js/test.js
@@ -1,23 +1,19 @@
-var name = 'Linn'
-
-var simon = {
-    name:'simon',
-    getName:()=>{
-        console.log(this)
-        console.log(this.name)
+function fbnq(n){
+    if(n < 0){
+        return 0
     }
+    if(n == 1){
+        return 1
+    }
+
+    let f0 = 0,f1 = 1
+    let f2 = 0
+    for(let i = 2;i <= n;i ++){
+        f2 = f0 + f1
+        f0 = f1
+        f1 = f2
+    }
+    return f2
 }
 
-var Simon = {
-    name:'simon',
-    getName:function(){
-        setTimeout(()=>{
-            console.log(this)
-            console.log(this.name)
-        },1000)
-    }
-}
-
-
-simon.getName()
-Simon.getName()
+console.log(fbnq(3))
