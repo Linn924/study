73ee5ffe7004cccc92964c83842171f51483df38
linncode <193265261@qq.com>
2020-10-20 21:58:24 +0800

    新增博客

--- 面试资料/js/test.js
+++ 面试资料/js/test.js
@@ -1,19 +1,3 @@
-function fbnq(n){
-    if(n < 0){
-        return 0
-    }
-    if(n == 1){
-        return 1
-    }
-
-    let f0 = 0,f1 = 1
-    let f2 = 0
-    for(let i = 2;i <= n;i ++){
-        f2 = f0 + f1
-        f0 = f1
-        f1 = f2
-    }
-    return f2
-}
-
-console.log(fbnq(3))
+var a = 888; 
+++a;
+console.log(a++)
